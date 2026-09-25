370b07f639f2d9e8fbc399a6ba89a233e0ec3255
Yacoob <yacoobkhan18@gmail.com>
2026-09-25 10:57:41 +0530

    Implemented Scroll View and button Components

--- CoreComponentsJS/App.js
+++ CoreComponentsJS/App.js
@@ -1,4 +1,4 @@
-import { View, Text,Image, ImageBackground } from "react-native";
+import { View, Text,Image, ImageBackground, ScrollView,Button } from "react-native";
 
 const logoImg = require('./assets/icon.png')
 
@@ -7,8 +7,16 @@ export default function App(){
     {/* <Image source={logoImg} style={{height:300,width:300}}/>
     <Image source={{uri: "https://picsum.photos/300"}} style={{height:300, width:300}}/>
     <Text><Text style={{color:"white"}}>Hello</Text> World</Text>  */}
-    <ImageBackground source={logoImg} style={{flex:1}}>
-      <Text>Hello World</Text>
-    </ImageBackground>
+    <ScrollView>
+      {/* <ImageBackground source={logoImg} style={{flex:1}}> */}
+      <Image source={logoImg} style={{height:300,width:300}}/>
+        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore.</Text>
+        <Image source={logoImg} style={{height:300,width:300}}/>
+        <Image source={logoImg} style={{height:300,width:300}}/>
+        <Image source={logoImg} style={{height:300,width:300}}/>
+      {/* </ImageBackground> */}
+    </ScrollView>
+
+    <Button title='Press' onPress={() => console.log("Button Pressed")} color="red"  disabled/>
   </View>
 }
