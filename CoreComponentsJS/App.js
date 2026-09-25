2c2f4d8a6e1c7dd782dc1464b3277345ffb24f9e
Yacoob <yacoobkhan18@gmail.com>
2026-09-25 10:28:21 +0530

    Image and ImageBackground components implemented

--- CoreComponentsJS/App.js
+++ CoreComponentsJS/App.js
@@ -1,7 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text,Image, ImageBackground } from "react-native";
+
+const logoImg = require('./assets/icon.png')
 
 export default function App(){
   return <View style={{flex:1 , backgroundColor:"plum", padding:60, }}>
-    <Text><Text style={{color:"white"}}>Hello</Text> World</Text> 
+    {/* <Image source={logoImg} style={{height:300,width:300}}/>
+    <Image source={{uri: "https://picsum.photos/300"}} style={{height:300, width:300}}/>
+    <Text><Text style={{color:"white"}}>Hello</Text> World</Text>  */}
+    <ImageBackground source={logoImg} style={{flex:1}}>
+      <Text>Hello World</Text>
+    </ImageBackground>
   </View>
 }
