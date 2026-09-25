6ab1e619618c8957fa2dd8318450a3501722404a
Yacoob <yacoobkhan18@gmail.com>
2026-09-25 13:16:00 +0530

    implemented the status bar component

--- CoreComponentsJS/App.js
+++ CoreComponentsJS/App.js
@@ -1,4 +1,4 @@
-import { View, Text,Image, ImageBackground, ScrollView,Button, Pressable, Modal } from "react-native";
+import { View, Text,Image, ImageBackground, ScrollView,Button, Pressable, Modal, StatusBar} from "react-native";
 import { useState } from "react";
 
 const logoImg = require('./assets/icon.png')
@@ -6,22 +6,23 @@ const logoImg = require('./assets/icon.png')
 export default function App(){
 
   const [isModalVisible,setIsModalVisible] = useState(false)
+  const [isStatusBar,setIsStatusBar] = useState(false)
 
   return <View style={{flex:1 , backgroundColor:"plum", padding:60, }}>
     {/* <Image source={logoImg} style={{height:300,width:300}}/>
     <Image source={{uri: "https://picsum.photos/300"}} style={{height:300, width:300}}/>
     <Text><Text style={{color:"white"}}>Hello</Text> World</Text>  */}
-    <ScrollView>
+    {/* <ScrollView> */}
       {/* <ImageBackground source={logoImg} style={{flex:1}}> */}
-      <Image source={logoImg} style={{height:300,width:300}}/>
+      {/* <Image source={logoImg} style={{height:300,width:300}}/>
         <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore.</Text>
         <Image source={logoImg} style={{height:300,width:300}}/>
         <Image source={logoImg} style={{height:300,width:300}}/>
-        <Image source={logoImg} style={{height:300,width:300}}/>
+        <Image source={logoImg} style={{height:300,width:300}}/> */}
       {/* </ImageBackground> */}
-    </ScrollView>
+    {/* </ScrollView> */}
 
-    <Button title='Press' onPress={() => console.log("Button Pressed")} color="red"  disabled/>
+    {/* <Button title='Press' onPress={() => console.log("Button Pressed")} color="red"  disabled/>
 
       <Pressable onPressOut={() => console.log("Image Pressed")}>
         <Image source={logoImg} style={{height:300,width:300}}/>
@@ -29,7 +30,16 @@ export default function App(){
 
       <Pressable onPressOut={() => console.log("Text Pressed")}>
         <Text>This is a Text, which check with pressable component.</Text>
-      </Pressable>
+      </Pressable> */}
+      <View style={{marginBottom:20}}> 
+        <View style={{marginBottom:20}}>
+             <Button title="StatusBar" onPress={() => setIsStatusBar(true)}/>
+        </View>
+      {/* <StatusBar backgroundColor="blue" barStyle="default" hidden={!isStatusBar}/> */}
+
+      <Button title="Close" onPress={()=>setIsStatusBar(false)}/>
+    
+    </View>
 
       <Button title="Open" onPress={() => setIsModalVisible(true)}/>
 
